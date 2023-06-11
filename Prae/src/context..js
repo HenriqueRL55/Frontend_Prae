cfc90176ccaceca86ea5ac440880ccb1c0442ab9
HenriqueRL55 <henriquerl22@outlook.com>
2023-06-11 19:10:30 -0300

    .

--- Prae/src/context..js
+++ Prae/src/context..js
@@ -1,13 +1,12 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useCallback } from 'react';
-const URL = "https://prae-backend-projeto.herokuapp.com/books/all";
+import React, { useState, useContext, useEffect, useCallback } from 'react';
+const URL = 'https://prae-backend-projeto.herokuapp.com/books/all';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [resultTitle, setResultTitle] = useState("");
+  const [resultTitle, setResultTitle] = useState('');
 
   const fetchBooks = useCallback(async () => {
     setLoading(true);
@@ -17,23 +16,28 @@ const AppProvider = ({ children }) => {
       const { books } = data;
 
       if (books.length > 0) {
-        setBooks(books);
-
-        if (books.length > 1) {
-          setResultTitle("Resultados de sua busca!");
+        if (searchTerm !== '') {
+          const filteredBooks = books.filter((book) =>
+            book.title.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+          setBooks(filteredBooks);
+          setResultTitle(
+            filteredBooks.length > 0 ? 'Resultados de sua busca!' : 'Sem resultados!'
+          );
         } else {
-          setResultTitle("Sem resultados!");
+          setBooks(books);
+          setResultTitle(books.length > 1 ? 'Resultados de sua busca!' : 'Sem resultados!');
         }
       } else {
         setBooks([]);
-        setResultTitle("Sem Resultados!");
+        setResultTitle('Sem resultados!');
       }
       setLoading(false);
     } catch (error) {
       console.log(error);
       setLoading(false);
     }
-  }, []);
+  }, [searchTerm]);
 
   useEffect(() => {
     fetchBooks();
@@ -47,6 +51,7 @@ const AppProvider = ({ children }) => {
         setSearchTerm,
         resultTitle,
         setResultTitle,
+        searchTitle: searchTerm,
       }}
     >
       {children}
